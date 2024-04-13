adbdb945bc8b599a7b84fce73bd79002d9ec9898
Nishant <nishantsurve8484@gmail.com>
2024-04-13 15:26:26 +0530

    added frontend project files

---
+++ src/components/reviews/Reviews.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Reviews.scss'
+import Review from '../review/Review'
+import { useQuery } from '@tanstack/react-query'
+const Reviews = ({gigId}) => {
+
+    const { isLoading, error, data} = useQuery({
+        queryKey: ["reviews"],
+        queryFn: () =>
+          newRequest.get(`/reviews/${gigId}`).then((res)=>{
+            return res.data;
+          })
+      });
+    
+
+  return (
+    <div className="reviews">
+    <h2>Reviews</h2>
+    <Review/>
+
+    { isLoading ? ("loading") :error ?( "Something error") : data.map(review =>
+    <Review key={review._id} review={review} />)}
+  </div>
+  )
+}
+
+export default Reviews
